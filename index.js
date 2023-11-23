7cd7223a0449d567812aee3b183479bf3df88ed9
Babyhulk89 <131919459+Babyhulk89@users.noreply.github.com>
2023-11-23 07:53:38 -0500

    index.js

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+// index.js
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      renderBooks(data);
+    })
+    .catch(error => {
+      console.error('Error fetching books:', error);
+      throw error; // Re-throw the error to propagate it to the caller or test
+    });
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+  const bookListElement = document.getElementById('bookList');
+
+  // Check if the target element exists before manipulating it
+  if (bookListElement) {
+    bookListElement.innerHTML = '';
+
+    books.forEach(book => {
+      const titleElement = document.createElement('li');
+      titleElement.textContent = book.title;
+      bookListElement.appendChild(titleElement);
+    });
+  } else {
+    console.error("Element with id 'bookList' not found in the DOM");
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+// Call the fetchBooks function when the script is executed
+fetchBooks();
